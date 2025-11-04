27a17d8b4782e69ef3145d46bfdd5b00dc883442
9osari <128437395+9osari@users.noreply.github.com>
2025-11-04 23:21:24 +0900

    feat: 입찰 로직 구현
    - 입찰 성공 페이지 추가

--- src/main/java/org/devbid/auction/application/AuctionApplicationService.java
+++ src/main/java/org/devbid/auction/application/AuctionApplicationService.java
@@ -4,16 +4,22 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.devbid.auction.domain.Auction;
 import org.devbid.auction.domain.AuctionFactory;
+import org.devbid.auction.domain.Bid;
+import org.devbid.auction.domain.BidAmount;
 import org.devbid.auction.dto.AuctionListResponse;
 import org.devbid.auction.dto.AuctionRegistrationRequest;
 import org.devbid.auction.repository.AuctionRepository;
+import org.devbid.auction.repository.BidRepository;
 import org.devbid.product.application.awsService.S3Service;
 import org.devbid.product.domain.Product;
 import org.devbid.product.domain.ProductImage;
 import org.devbid.product.repository.ProductRepository;
+import org.devbid.user.domain.User;
+import org.devbid.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.Comparator;
 import java.util.List;
 
@@ -24,6 +30,8 @@ import java.util.List;
 public class AuctionApplicationService implements AuctionService{
     private final ProductRepository productRepository;
     private final AuctionRepository auctionRepository;
+    private final UserRepository userRepository;
+    private final BidRepository bidRepository;
     private final AuctionDtoMapper auctionDtoMapper;
     private final S3Service s3Service;
 
@@ -89,6 +97,17 @@ public class AuctionApplicationService implements AuctionService{
                 .sorted(Comparator.comparing(ProductImage::getSortOrder))
                 .map(img -> s3Service.buildPublicUrl(img.getImageKey()))
                 .toList();
+    }
+
+    @Override
+    public void placeBid(Long auctionId, Long bidderId, BigDecimal bidAmount) {
+        Auction auction = findById(auctionId);  //경매찾기
+        User bidder = userRepository.findById(bidderId) //사용자 찾기
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+
+        //도메인 객체에 비즈니스 로직 위임
+        Bid bid = auction.placeBid(bidder, new BidAmount(bidAmount));
+        bidRepository.save(bid);
     }
 
 }
